441ef7485b72532a8783556bb0f4219666674702
14metehan53 <14metehan53@gmail.com>
2025-05-09 19:01:35 +0300

    first data from API

--- app/api/crypto/ticker/route.ts
+++ app/api/crypto/ticker/route.ts
@@ -1,15 +1,14 @@
 import { NextResponse } from 'next/server';
 import { CryptoData } from '@/types/ticker';
 
-let cachedData: CryptoData | null = null;
-let lastFetched: number = 0;
-const BUFFER_TIME = 30 * 1000; // extra 30 sec
+let cachedData: CryptoData[] | null = null;
+const BUFFER_TIME: number = 30 * 1000; // extra 30 sec
 
 export async function GET() {
-  const now = Date.now();
-  const nowDate = new Date();
+  const now: number = Date.now();
+  const nowDate: Date = new Date();
 
-  const nextUpdate =
+  const nextUpdate: number =
     new Date(
       nowDate.setMinutes(Math.ceil(nowDate.getMinutes() / 5) * 5, 0, 0)
     ).getTime() + BUFFER_TIME;
@@ -19,20 +18,19 @@ export async function GET() {
     return NextResponse.json(cachedData);
   }
 
-  const apiUrl = process.env.TICKER
+  const apiUrl: string = process.env.TICKER
     ? `${process.env.TICKER}?limit=4`
     : 'https://api.alternative.me/v1/ticker/?limit=4';
 
   try {
-    const response = await fetch(apiUrl);
+    const response: Response = await fetch(apiUrl);
     if (!response.ok) {
       throw new Error('API request failed.');
     }
 
-    const data = await response.json();
+    const data: CryptoData[] = await response.json();
 
     cachedData = data;
-    lastFetched = now;
     console.log('New data was pulled from the API.');
 
     return NextResponse.json(data);
